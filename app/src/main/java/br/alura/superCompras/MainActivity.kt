1120ab634d2f2e827bce778e347d00107b68b47e
Vitor gabriel <vitorgabrieltrabalho@gmail.com>
2025-07-16 12:37:13 -0300

    adcionando_botões_checkbox

--- app/src/main/java/br/alura/superCompras/MainActivity.kt
+++ app/src/main/java/br/alura/superCompras/MainActivity.kt
@@ -5,24 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.alura.superCompras.ui.theme.SuperComprasTheme
+import br.alura.superCompras.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,62 +32,106 @@ class MainActivity : ComponentActivity() {
         setContent {
             SuperComprasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ImagemTopo(modifier = Modifier.padding(innerPadding))
-                    icone(modifier  = Modifier.padding(innerPadding),
-                        icone =  Icons.Default.Delete)
+                    Column {
+                        imagemTopo(modifier = Modifier.padding(innerPadding))
+                        Titulo(texto = "Gerenciando Clientes", modifier = Modifier.padding(innerPadding))
+                        Titulo(texto = "Escolha qual seleção", modifier = Modifier.padding(innerPadding))
+                        itemDaLista(modifier = Modifier.padding(innerPadding))
+
+
+
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun ImagemTopo(modifier: Modifier = Modifier) {
-    Image(painter = painterResource(R.drawable.logo),
-        contentDescription = null,
-        modifier = modifier.size(160.dp)
-    )
-}
-
-
-@Composable
-fun icone (icone: ImageVector,modifier: Modifier = Modifier) {
-    Icon(icone, contentDescription = "Editar", modifier = modifier)
-}
-
-@Preview
-@Composable
-private fun icone() {
-    SuperComprasTheme {
-        icone(Icons.Default.Delete)
+    @Composable
+    fun imagemTopo(modifier: Modifier = Modifier) {
+        Image(
+            painter = painterResource(R.drawable.logo),
+            contentDescription = null,
+            modifier = modifier.size(160.dp)
+        )
     }
 
-}
+    @Preview
+    @Composable
+    private fun imagemTopo() {
+        SuperComprasTheme {
+            imagemTopo()
+        }
+        
+    }
 
-@Preview
-@Composable
-private fun imagemPreview() {
-    SuperComprasTheme {
-        ImagemTopo()
+
+    @Composable
+    fun icone(icone: ImageVector, modifier: Modifier = Modifier) {
+        Icon(icone, contentDescription = "Editar", modifier = modifier)
+    }
+
+
+
+    @Composable
+    fun itemDaLista(modifier: Modifier = Modifier) {
+        Row {
+
+        Checkbox(checked = false, onCheckedChange = {}, modifier = modifier)
+            Text(text = "Suco", style = Typography.bodyMedium,modifier = modifier)
+            Checkbox(checked = false, onCheckedChange = {}, modifier = modifier)
+            Text(text = "Suco", style = Typography.bodyMedium,modifier = modifier)
+            Checkbox(checked = false, onCheckedChange = {}, modifier = modifier)
+            Text(text = "Suco", style = Typography.bodyMedium,modifier = modifier)
+        }
+
+    }
+
+    @Preview
+    @Composable
+    private fun itemDaLista() {
+        SuperComprasTheme {
+            itemDaLista()
+        }
+
+    }
+
+
+    @Composable
+    fun Titulo(texto:String, modifier: Modifier = Modifier) {
+        Text(
+            text = texto, style = Typography.headlineLarge, modifier = modifier
+        )
+
+    }
+
+    @Preview
+    @Composable
+    private fun TituloPreview() {
+        SuperComprasTheme {
+            Titulo(texto = " Gerenciamneto")
+        }
+
+    }
+
+    @Preview
+    @Composable
+    private fun icone() {
+        SuperComprasTheme {
+            icone(Icons.Default.Delete)
+        }
+
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        SuperComprasTheme {
+            Greeting("Android")
+        }
     }
 }
 
 
-@Composable
-fun Titulo(modifier: Modifier = Modifier) {
-    Text(
-        text ="Lista de teste", modifier = modifier)
-
-}
-
-@Preview
-@Composable
-private fun TituloPreview() {
-    SuperComprasTheme {
-        Titulo()
-    }
-
-}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -95,10 +140,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SuperComprasTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/br/alura/superCompras/ui/theme/Type.kt
+++ app/src/main/java/br/alura/superCompras/ui/theme/Type.kt
@@ -11,6 +11,7 @@ val Typography = Typography(
     headlineLarge = TextStyle(
         fontFamily = FontFamily.Serif,
         fontWeight = FontWeight.Normal,
+        color = marinho,
         fontSize = 20.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
@@ -18,22 +19,25 @@ val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Monospace,
         fontWeight = FontWeight.Normal,
-        fontSize = 18.sp,
-        lineHeight = 24.sp,
+        color = azulClaro,
+        fontSize = 10.sp,
+        lineHeight = 14.sp,
         letterSpacing = 0.5.sp
     ),
     bodyMedium = TextStyle(
         fontFamily = FontFamily.SansSerif,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+        color = azulClaro,
+        fontSize = 20.sp,
+        lineHeight = 20.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp,
-        lineHeight = 20.sp,
+        color = marinho,
+        fontSize = 25.sp,
+        lineHeight = 30.sp,
         letterSpacing = 0.5.sp
     )
 
